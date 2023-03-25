bb9f8b0983c3bd1c162d5adee74cddb8f0213e26
alxmart <alxmartinez@gmail.com>
2023-03-25 13:32:50 -0300

    Added textviews and code to finish the second activity.

--- app/src/main/java/com/example/aulaactivityfragment/DetalhesActivity.kt
+++ app/src/main/java/com/example/aulaactivityfragment/DetalhesActivity.kt
@@ -1,11 +1,23 @@
 package com.example.aulaactivityfragment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DetalhesActivity : AppCompatActivity() {
+
+    lateinit var buttonFechar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhes)
+
+        buttonFechar = findViewById(R.id.btn_fechar)
+
+        buttonFechar.setOnClickListener  {
+
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/example/aulaactivityfragment/MainActivity.kt
+++ app/src/main/java/com/example/aulaactivityfragment/MainActivity.kt
@@ -22,12 +22,6 @@ class MainActivity : AppCompatActivity() {
             )
 
             startActivity(intent)
-
         }
-
-
-
-
-
     }
 }
