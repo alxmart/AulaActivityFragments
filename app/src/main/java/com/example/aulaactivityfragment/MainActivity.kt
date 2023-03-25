dcfd3ce35de7be445066f059c24843e66d643add
alxmart <alxmartinez@gmail.com>
2023-03-25 11:34:01 -0300

    Defining startActivity when click button.

--- app/src/main/java/com/example/aulaactivityfragment/MainActivity.kt
+++ app/src/main/java/com/example/aulaactivityfragment/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.aulaactivityfragment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var buttonAbrir: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonAbrir = findViewById(R.id.btn_abrir)
+
+        buttonAbrir.setOnClickListener  {
+
+            val intent = Intent(
+                this,
+                DetalhesActivity::class.java
+            )
+
+            startActivity(intent)
+
+        }
+
+
+
+
+
     }
 }
